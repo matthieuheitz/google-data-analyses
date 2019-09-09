15a6d77da40ee745c4500e87684269f7a9f14abc
Matthieu Heitz <heitz.matthieu@gmail.com>
2019-09-09 20:14:29 +0200

    Add time groups
    Comment activity to save memory

--- get_visit_times.py
+++ get_visit_times.py
@@ -44,8 +44,9 @@ def dist_btw_two_points(p1,p2):
 
 json_file = "location_history.json"
 # Read the file
+print("Loading '%s' ..."%json_file)
 main_dict = json.load(open(json_file))   # This can take a bit of time
-
+print("JSON file loaded")
 # data is a big list of dicts.
 data = main_dict['locations']
 n = len(data)   # Number of timesteps
@@ -62,38 +63,57 @@ n = len(data)   # Number of timesteps
 # print(data[-1])
 
 # Build arrays of basic data
+print("Extracting relevant data...")
 timestampMs = np.zeros(n)   # in milliseconds
 positions = np.zeros([n,2])  # in degrees
 accuracy = np.zeros(n)      # in %
-activity = {}
+# activity = {}         # Don't store activity since we don't use it
 for i in range(n):
     point = data[i]
     timestampMs[i] = float(point['timestampMs'])
     positions[i] = np.array([float(point['latitudeE7']),float(point['longitudeE7'])])/1e7
     accuracy[i] = point['accuracy']
-    if 'activity' in point: activity[i] = point['activity']
+    # if 'activity' in point: activity[i] = point['activity']
 
-n_act = len(activity.keys())
+# n_act = len(activity.keys())
+# print("Total of points with activity: %d  (%0.1f%%)"%(n_act,int(n_act/n*100)))
 print("Total of points: %d"%n)
-print("Total of points with activity: %d  (%0.1f%%)"%(n_act,int(n_act/n*100)))
 
+# Free some memory
+data.clear()
+main_dict.clear()
 
+#############
 # USER INPUT
+#############
 
 # Get first and last timestamps of interest
 begin_ts = date_ymd_to_timestamp_ms(2018,9,1)
-end_ts = date_ymd_to_timestamp_ms(2019,3,1)
+end_ts = date_ymd_to_timestamp_ms(2018,9,30)
 # end_ts = timestampMs[-1]    # Last one
 
 # Point of interest
 poi = np.array([45.773944,4.890715])    # in degrees
 radius_max = 50                         # in meters
 
+# Define the interval of time below which timestamps should be grouped together
+group_size = datetime.timedelta(weeks=0, days=0, hours=1, minutes=0, seconds=0, milliseconds=0)
+# Amount of info to show about each group:
+# 0: None
+# 1: Number of points in group
+# 2: Add point IDs, first/last datetime in group, avg dist to POI
+group_verbosity = 1
 
+####################
+# END OF USER INPUT
+####################
 
 # Get time boundary index
 begin_index = np.argmax(timestampMs >= begin_ts)
 end_index = np.argmax(timestampMs >= end_ts)
+
+# Get group_size in milliseconds
+grpsMs =  group_size.total_seconds()*1000
 
 # Get all points that are within radius_max of the poi
 close_points = []
@@ -109,6 +129,28 @@ for i in range(begin_index,end_index):
 close_points = np.array(close_points)
 print("Number of close points: %d"%close_points.size)
 
+ts2datetime = lambda x: datetime.datetime.utcfromtimestamp(int(x/1e3)).strftime('%Y-%m-%d %H:%M:%S')
+
+prev = 0    # Keeps in memory the last timestamp displayed
 for i in range(close_points.size):
-    pt_date = datetime.datetime.utcfromtimestamp(int(timestampMs[close_points[i]]/1e3)).strftime('%Y-%m-%d %H:%M:%S')
-    print("Point %d  --  Date: %s  --  Distance to POI: %dm"%(close_points[i],pt_date,dist2poi[i]))
+    # If the delta between timestamps is bigger than the group size, or if it's the beginning or the end.
+    if timestampMs[close_points[i]]-timestampMs[close_points[prev]] > grpsMs or i == 0 or i == close_points.size-1:
+        # If it's not the beginning and there are at least 2 points to make a group.
+        if i>0 and i-prev > 2:
+            if i == close_points.size-1: i=i+1
+            if group_verbosity == 1:
+                print("\tGroup of %d points"%(i-prev-2))
+            if group_verbosity == 2:
+                print("\tGroup of %d points: %d -> %d"%(i-prev-2,close_points[prev+1],close_points[i-1]))
+                pt_date_im1 = ts2datetime(timestampMs[close_points[i-1]])
+                pt_date_prevp1 = ts2datetime(timestampMs[close_points[prev+1]])
+                print("\tFrom: %s"%pt_date_prevp1)
+                print("\tTo  : %s"%pt_date_im1)
+                print("\tMean dist to POI: %0.1fm"%np.mean(dist2poi[prev+1:i]))
+
+        if group_verbosity == 2: print()    # Add space between lines
+        # Else, display the point, unless it's the end
+        if i != close_points.size:
+            pt_date = ts2datetime(timestampMs[close_points[i]])
+            print("Point %d  --  Date: %s  --  Distance to POI: %dm"%(close_points[i],pt_date,dist2poi[i]))
+            prev = i
